0a8ca44a974448ce99921e5102d2aff5177d26e5
gauravjha78 <gauravjha9878@gmail.com>
2026-04-14 12:49:10 +0530

    Switched email service from SMTP to Resend API

--- backend/app/services/email_service.py
+++ backend/app/services/email_service.py
@@ -1,25 +1,44 @@
 # Seniding the email to the user
 from dotenv import load_dotenv
 import os
-import smtplib
-from email.mime.text import MIMEText
+import resend
+# import smtplib
+# from email.mime.text import MIMEText
+
+resend.api_key=os.getenv("RESEND_API_KEY")
+load_dotenv()
+
+# THIS IS WHEN WE USE GMAIL AND WE HAVE AWS CLOUD AS BACKEND PLATFORM.
+# def send_emails(to_email, subject , body):
+#     sender_email=os.getenv("EMAIL_USER")
+#     sender_password=os.getenv("EMAIL_PASS")
+
+# # ✅ ensure everything is string
+#     subject = str(subject)
+#     body = str(body)
 
 
-def send_emails(to_email, subject , body):
-    sender_email=os.getenv("EMAIL_USER")
-    sender_password=os.getenv("EMAIL_PASS")
+#     msg=MIMEText(body)
+#     msg["Subject"]=subject
+#     msg["From"]=sender_email
+#     msg["To"]=to_email
 
-# ✅ ensure everything is string
-    subject = str(subject)
-    body = str(body)
+#     with smtplib.SMTP("smtp.gmail.com",587) as server:
+#         server.starttls()
+#         server.login(sender_email,sender_password)
+#         server.send_message(msg)
 
+#  This is for resend the platfrom for poduction email
+def send_emails(to_email, subject, body):
+    try:
+        resend.Emails.send({
+            "from": "onboarding@resend.dev",
+            "to": to_email,
+            "subject": subject,
+            "html": body
+        })
 
-    msg=MIMEText(body)
-    msg["Subject"]=subject
-    msg["From"]=sender_email
-    msg["To"]=to_email
+        print("✅ Email sent successfully")
 
-    with smtplib.SMTP("smtp.gmail.com",587) as server:
-        server.starttls()
-        server.login(sender_email,sender_password)
-        server.send_message(msg)
+    except Exception as e:
+        print("❌ Email failed:", e)
