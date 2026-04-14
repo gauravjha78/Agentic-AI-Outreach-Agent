93337043b8652c4bd553c32a803812a0ef439cb3
gauravjha78 <gauravjha9878@gmail.com>
2026-04-14 13:18:19 +0530

    Switched to Gmail SMTP for Render deployment

--- backend/app/services/email_service.py
+++ backend/app/services/email_service.py
@@ -1,44 +1,44 @@
 # Seniding the email to the user
 from dotenv import load_dotenv
 import os
-import resend
-# import smtplib
-# from email.mime.text import MIMEText
+# import resend
+import smtplib
+from email.mime.text import MIMEText
 
-resend.api_key=os.getenv("RESEND_API_KEY")
+# resend.api_key=os.getenv("RESEND_API_KEY")
 load_dotenv()
 
 # THIS IS WHEN WE USE GMAIL AND WE HAVE AWS CLOUD AS BACKEND PLATFORM.
-# def send_emails(to_email, subject , body):
-#     sender_email=os.getenv("EMAIL_USER")
-#     sender_password=os.getenv("EMAIL_PASS")
+def send_emails(to_email, subject , body):
+    sender_email=os.getenv("EMAIL_USER")
+    sender_password=os.getenv("EMAIL_PASS")
 
-# # ✅ ensure everything is string
-#     subject = str(subject)
-#     body = str(body)
+# ✅ ensure everything is string
+    subject = str(subject)
+    body = str(body)
 
 
-#     msg=MIMEText(body)
-#     msg["Subject"]=subject
-#     msg["From"]=sender_email
-#     msg["To"]=to_email
+    msg=MIMEText(body)
+    msg["Subject"]=subject
+    msg["From"]=sender_email
+    msg["To"]=to_email
 
-#     with smtplib.SMTP("smtp.gmail.com",587) as server:
-#         server.starttls()
-#         server.login(sender_email,sender_password)
-#         server.send_message(msg)
+    with smtplib.SMTP("smtp.gmail.com",587) as server:
+        server.starttls()
+        server.login(sender_email,sender_password)
+        server.send_message(msg)
 
 #  This is for resend the platfrom for poduction email
-def send_emails(to_email, subject, body):
-    try:
-        resend.Emails.send({
-            "from": "onboarding@resend.dev",
-            "to": to_email,
-            "subject": subject,
-            "html": body
-        })
+# def send_emails(to_email, subject, body):
+#     try:
+#         resend.Emails.send({
+#             "from": "onboarding@resend.dev",
+#             "to": to_email,
+#             "subject": subject,
+#             "html": body
+#         })
 
-        print("✅ Email sent successfully")
+#         print("✅ Email sent successfully")
 
-    except Exception as e:
-        print("❌ Email failed:", e)
+#     except Exception as e:
+#         print("❌ Email failed:", e)
